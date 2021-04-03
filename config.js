58a54cbdb9539904bdd8498bfd0e4becddbbcb2d
oebitw <engomarewies@gmail.com>
2021-04-03 20:11:50 +0300

    DONE

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "7cb46b0d74ca326ac29efd0d5eba3002"
+    }
+}
+
+module.exports = constants;
+
